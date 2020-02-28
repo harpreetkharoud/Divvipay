e1b5f064fcf8b8d1e5f8c4fb5fc13720e546ab2e
harpreetkaur <harpreetkaroud90@gmail.com>
2020-02-27 23:41:05 -0800

    signup update commit

--- app/src/main/java/com/example/divvipay/splash.java
+++ app/src/main/java/com/example/divvipay/splash.java
@@ -2,7 +2,9 @@ package com.example.divvipay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class splash extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(splash.this, Login.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+
     }
 }
