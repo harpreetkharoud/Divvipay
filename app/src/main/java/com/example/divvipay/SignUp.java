863642dfdcd9b5afd22ff7664f0350aff82971df
chirag3231 <chirag3231@gmail.com>
2020-03-13 14:51:09 -0700

    Comit

--- app/src/main/java/com/example/divvipay/SignUp.java
+++ app/src/main/java/com/example/divvipay/SignUp.java
@@ -14,6 +14,7 @@ public class SignUp extends AppCompatActivity {
     EditText Password1;
     EditText Password2;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
